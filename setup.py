275939f1b8c887a115bfdfd29be59f06d2ba53aa
Andreas Klintberg <andreas.klintberg@meltwater.com>
2017-09-29 00:40:53 -0700

    fixed dependencies in setup py

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 with open('README.md') as file:
     long_description = file.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
     name='automile_api',
@@ -13,5 +16,7 @@ setup(
     long_description=long_description,
     url='https://api.automile.com',
     license='MIT',
+    packages=find_packages(),
+    install_requires=required,
     py_modules=['automile/automile_api'],
 )
